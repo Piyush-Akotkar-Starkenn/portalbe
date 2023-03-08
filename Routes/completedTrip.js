0d82d018669655d064f1b9cdc73cc91e19fae339
piyushak21 <106967130+piyushak21@users.noreply.github.com>
2023-03-08 19:24:34 +0530

    update completedTrip route

--- Routes/completedTrip.js
+++ Routes/completedTrip.js
@@ -4,6 +4,7 @@ import {
   getCompletedTripsAll,
   getFaultCountByTripId,
   getTripDataById,
+  getCompletedTripsByVehicleId,
 } from "../Controller/CompletedTripController.js";
 
 const CompletedTripRoute = express.Router();
@@ -12,5 +13,9 @@ CompletedTripRoute.get("/getTripById/:id", getTripDataById);
 CompletedTripRoute.get("/getFaultsByTripId/:id", getFaultCountByTripId);
 CompletedTripRoute.get("/getCompletedTrips/:offset", getCompletedTrips);
 CompletedTripRoute.get("/getCompletedTrips/", getCompletedTripsAll);
+CompletedTripRoute.get(
+  "/getCompletedTripsByVehicleId/:id",
+  getCompletedTripsByVehicleId
+);
 
 export default CompletedTripRoute;
