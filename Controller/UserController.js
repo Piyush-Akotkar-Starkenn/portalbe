f1dae9800650747f63129715fd31363880336aaa
piyushak21 <106967130+piyushak21@users.noreply.github.com>
2023-03-10 18:21:10 +0530

    Create UserController.js

---
+++ Controller/UserController.js
@@ -0,0 +1,12 @@
+import { db } from "../Config/db";
+
+export default getAllUsers = (req, res) => {
+  const q =
+    "SELECT user_id, first_name, last_name, status FROM users WHERE status = 1";
+  db.query(q, (err, results) => {
+    if (err) return err;
+    if (results) {
+      return res.status(200).send(results);
+    }
+  });
+};
