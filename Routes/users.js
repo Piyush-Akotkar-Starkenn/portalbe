b83a0cd9c0bdda423042e47e7e7ac701eebc45d2
piyushak21 <106967130+piyushak21@users.noreply.github.com>
2023-03-13 17:24:06 +0530

    Update users

--- Routes/users.js
+++ Routes/users.js
@@ -2,6 +2,6 @@ import express from "express";
 import { getAllUsers } from "../Controller/UserController.js";
 const UsersRouter = express.Router();
 
-UsersRouter.get("/getall", getAllUsers);
+UsersRouter.get("/get-all-users", getAllUsers);
 
 export default UsersRouter;
