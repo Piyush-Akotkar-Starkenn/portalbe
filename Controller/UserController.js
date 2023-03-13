90e71cdc70cb000c57b574788b98bd93768f0c80
piyushak21 <106967130+piyushak21@users.noreply.github.com>
2023-03-13 13:13:44 +0530

    Update user controller

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -1,6 +1,6 @@
 import { db } from "../Config/db.js";
 
-export default getAllUsers = (req, res) => {
+export const getAllUsers = (req, res) => {
   const q =
     "SELECT user_id, first_name, last_name, status FROM users WHERE status = 1";
   db.query(q, (err, results) => {
