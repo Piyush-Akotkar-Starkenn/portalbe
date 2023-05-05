b4e2815351e00d06b56e87cf9f2624acab0c3aee
harshal655 <127827603+harshal655@users.noreply.github.com>
2023-05-05 18:17:46 +0530

    login active deactive updated

--- Controller/LoginController.js
+++ Controller/LoginController.js
@@ -5,42 +5,57 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export const login = (req, res) => {
-  const getDataQuery = "SELECT * FROM users WHERE email = ? ";
+  const CheckEmail = "SELECT * FROM users WHERE email = ?";
 
-  db.query(getDataQuery, [req.body.email], (err, data) => {
+  db.query(CheckEmail, [req.body.email], (err, results) => {
     if (err) {
-      return res.status(500).send({ Error: err });
+      res.status(500).send({ Error: err });
     } else {
-      if (data.length > 0) {
-        const hash = data[0].password;
+      if (results.length == 0) {
+        res.status(500).send({ Error: "Wrong Email" });
+      } else {
+        const CheckStatus = "SELECT * FROM users WHERE email=? AND status=1";
 
-        bcrypt.compare(req.body.password, hash, function (err, result) {
+        db.query(CheckStatus, [req.body.email], (err, data) => {
           if (err) {
-            return res.status(500).send({ ErrorPassComp: err });
+            res.staus(500).send({ Error: err });
           } else {
-            if (result) {
-              const token = jwt.sign(
-                { id: data[0].id },
-                process.env.JWT_SECRET
-              );
-
-              return res.status(200).send({
-                Message: "Login Successfull",
-                token,
-                user_type: data[0].user_type,
-                user_id: data[0].user_id,
-              });
+            if (data.length == 0) {
+              res.status(500).send({ Error: "User Deactivated" });
             } else {
-              return res.status(500).send({ ErrorPass: "Wrong Password" });
+              const hash = data[0].password;
+
+              bcrypt.compare(req.body.password, hash, function (err, result) {
+                if (err) {
+                  return res.status(500).send({ ErrorPassComp: err });
+                } else {
+                  if (result) {
+                    const token = jwt.sign(
+                      { id: data[0].id },
+                      process.env.JWT_SECRET
+                    );
+
+                    return res.status(200).send({
+                      Message: "Login Successfull",
+                      token,
+                      user_type: data[0].user_type,
+                      user_id: data[0].user_id,
+                    });
+                  } else {
+                    return res
+                      .status(500)
+                      .send({ ErrorPass: "Wrong Password" });
+                  }
+                }
+              });
             }
           }
         });
-      } else {
-        return res.status(500).send("Wrong Email");
       }
     }
   });
 };
+
 
 export const signup = (req, res) => {
   const checkuserQuery = "SELECT * FROM users WHERE  username=? OR email=? ";
