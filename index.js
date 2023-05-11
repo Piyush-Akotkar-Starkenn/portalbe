fcfee2fd922006df133486c0c906ec0ac0b1ab78
piyushak21 <106967130+piyushak21@users.noreply.github.com>
2023-05-11 15:47:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ import OngoingTripsRouter from "./Routes/ongoingTrips.js";
 import CompletedTripRoute from "./Routes/completedTrip.js";
 import UsersRouter from "./Routes/users.js";
 import cookieParser from "cookie-parser";
-import { spawn } from "child_process";
 
 const app = express();
 app.use(express.json());
@@ -19,8 +18,8 @@ app.use(cookieParser());
 app.use(cors({ origin: "*" }));
 getMqttData();
 
-cronJob();
-// setInterval(cronJob, 30 * 60 * 1000); // run cronjob every 30 mins
+// cronJob();
+setInterval(cronJob, 30 * 60 * 1000); // run cronjob every 30 mins
 
 app.use("/api/login", LoginRouter);
 app.use(authetication);
@@ -31,24 +30,6 @@ app.use("/api/completedTrip", CompletedTripRoute);
 app.use("/api/ongoingTrip", OngoingTripsRouter);
 app.use("/api/users", UsersRouter);
 
-app.get("/hello", (req, res) => {
-  let output = "";
-
-  const pythonProcess = spawn("python", ["-u", "pyModels/yunet_v_5.1.0.py"]);
-
-  pythonProcess.stdout.on("data", (data) => {
-    output += data.toString();
-  });
-
-  pythonProcess.stderr.on("data", (data) => {
-    console.error(`stderr: ${data}`);
-  });
-
-  pythonProcess.on("close", (code) => {
-    console.log(`child process exited with code ${code}`);
-    res.send(output);
-  });
-});
 
 app.listen(9001, () => {
   console.log("Listening on Port 9001");
